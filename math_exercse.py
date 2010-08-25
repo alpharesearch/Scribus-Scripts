b139b1f0064c750e7a98833f727324528dd8caf3
Markus Schulz <schulz@alpharesearch.de>
2010-08-25 19:38:02 -0400

    swaped solution rows

--- math_exercse.py
+++ math_exercse.py
@@ -11,9 +11,10 @@ pageY = 11.0
 maxX = 2.0
 maxY = 12.0
 offsetX = 0.3
-offsetXX = 0.3
+offsetXX = 0.2
 offsetY = 0.15
-maxResult = 9
+maxResultA = 15
+maxResultB = 2
 rx = [[0 for col in range(100)] for row in range(100)]
 ry = [[0 for col in range(100)] for row in range(100)]
 
@@ -22,25 +23,27 @@ scribus.defineColor("gray",21,21,21,21)
 
 for x in range(0,maxX):
 	for y in range(0,maxY):
-		q = scribus.createText((x*(pageX/maxX))+offsetX, (y*(pageY/maxY))+offsetY, pageX/maxX, pageY/maxY)
+		q = scribus.createText((x*((pageX*0.9)/maxX))+offsetX, (y*(pageY/maxY))+offsetY, (pageX*0.9)/maxX, pageY/maxY)
 		scribus.setFont('Arial Regular',q)
 		scribus.setFontSize(48, q)
 #		scribus.setTextAlignment(scribus.ALIGN_CENTERED, q)
 #		rx[x][y] = random.randint(0, maxResult)
 #		ry[x][y] = random.randint(0, (maxResult-rx[x][y]))
-		rx[x][y] = random.randint(0, maxResult)
-		ry[x][y] = random.randint(0, maxResult)
+		rx[x][y] = random.randint(0, maxResultA)
+		ry[x][y] = random.randint(0, maxResultB)
 		rxx = rx[x][y]
 		ryy = ry[x][y]
 		scribus.insertText('%(rxx)d + %(ryy)d =__' % locals(), 0, q)
 scribus.newPage(-1)
-for x in range(0,maxX):
+xx = 0
+for x in range(maxX-1,-1,-1):
 	for y in range(0,maxY):
-		q = scribus.createText((x*((pageX/2)/maxX))+offsetXX, (y*(pageY/maxY))+offsetY, (pageX/2)/maxX, pageY/maxY)
+		q = scribus.createText((xx*((pageX/6)/maxX))+offsetXX, (y*(pageY/maxY))+offsetY, (pageX/6)/maxX, pageY/maxY)
 		scribus.setFont('Arial Regular',q)
 		scribus.setFontSize(20, q)
 		scribus.setTextColor("gray", q)
 		rr = rx[x][y] + ry[x][y]
 #		scribus.setTextAlignment(scribus.ALIGN_CENTERED, q)
 		scribus.insertText('%(rr)d' % locals(), 0, q)
+	xx+=1
 
